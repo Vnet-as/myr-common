1c16e6a13950b9f52815338d5476d7011a640e8b
Michal Kuffa <michal.kuffa@vnet.sk>
2016-12-14 22:56:58 +0100

    Add myr.common.defaults module with discovery_queue as first default setting
    Signed-off-by: Michal Kuffa <michal.kuffa@vnet.sk>

---
+++ myr/common/defaults.py
@@ -0,0 +1,5 @@
+
+#: Default queue for discovery / announce tasks
+#: myr.discovery consumes this queue by default therefore handling announce
+#: tasks of myr.base workers and discovery tasks of myr.client
+discovery_queue = '_myr_discovery_'
